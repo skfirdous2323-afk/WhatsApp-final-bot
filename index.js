d92567f89f5ca594181c36f133b8a798b59d6170
Sk Firdous <skfirdous2323@gmaill.com>
2026-04-30 12:03:17 +0530

    first commit

--- index.js
+++ index.js
@@ -111,6 +111,37 @@ app.post("/checkin", async (req, res) => {
   }
 });
 
+// 🔥 IMPORTANT (VERIFY ROUTE)
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "12345";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  console.log("VERIFY HIT"); // 👈 log check
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("SUCCESS");
+    res.status(200).send(challenge);
+  } else {
+    console.log("FAILED");
+    res.sendStatus(403);
+  }
+});
+
+// POST route (baad me use hoga)
+app.post("/webhook", (req, res) => {
+  console.log("MSG:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("RUNNING"));
+
+
+
+
+
 
 // 🚀 Start server
 const PORT = process.env.PORT || 5000;
