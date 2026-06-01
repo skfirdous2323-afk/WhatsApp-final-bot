2d9d56e005bdc725ef126a08a6839f095bff68db
Sk Firdous <skfirdous2323@gmaill.com>
2026-06-01 22:25:29 +0530

    first commit

--- index.js
+++ index.js
@@ -564,6 +564,27 @@ data:dates
 
 
 
+app.get("/webhook", (req, res) => {
+  const verify_token = process.env.VERIFY_TOKEN;
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === verify_token) {
+    console.log("WEBHOOK VERIFIED");
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
+
+
+
+
+
+
+
 // Start server
 const PORT = process.env.PORT || 3000;
 
